0e42b8d908c7011b3e74f873139f3b090629e88d
Akshansh Rajput <ext_akraj@arlafoods.com>
2023-06-30 22:00:37 +0300

    added sample config with dynamic task creation

--- airflow/dags/dag_generator.py
+++ airflow/dags/dag_generator.py
@@ -3,25 +3,43 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 
 from airflow import DAG
 from airflow.providers.papermill.operators.papermill import PapermillOperator
+import yaml
+import os
 
+for filename in os.listdir('/opt/airflow/dags/config/'):
+    if filename.endswith('.yml'):
+        with open(f'/opt/airflow/dags/config/{filename}') as fi:
+            conf_file = yaml.safe_load(fi)
 
-with DAG(
-    dag_id='example_papermill_operator1',
-    default_args={
-        'retries': 0
-    },
-    schedule='0 0 * * *',
-    start_date=datetime(2022, 10, 1),
-    catchup=False
-) as dag_1:
+            dag_name = conf_file['dag_name']
 
-    # notebook_task = PapermillOperator(
-    #     task_id="run_example_notebook",
-    #     input_nb="dags/deployment/kafka_manager.ipynb",
-    #     output_nb="dags/deployment/output/kafka_manager.ipynb",
-    #     parameters={"KAFKA_BROKER_SERVER": "broker:29092","KAFKA_TOPIC_NAME":"test_air"},
-    # )
-    
-    from deployment.task.task import read_count
-    f = read_count.override(task_id="id1")()
-    f
+            with DAG(
+                dag_id=dag_name,
+                default_args={
+                    'retries': 0
+                },
+                schedule='0 0 * * *',
+                start_date=datetime(2022, 10, 1),
+                catchup=False
+            ) as dag_1:
+
+                from deployment.task.task import pipeline_builder
+                t_data = []
+                data_processors = conf_file['data_processor']
+                for data_processor in data_processors:
+                    task_name = data_processor['name']
+                    data_task = pipeline_builder.override(task_id=task_name)(data_processor)
+                    t_data.append(data_task)
+                need_kafka_topic = conf_file['need_kafka_topic']
+                if need_kafka_topic:
+                    topic_name = conf_file['topic_name']
+                    server = conf_file['kafka_server']
+                    notebook_task = PapermillOperator(
+                        task_id=f'create_topic',
+                        input_nb="dags/deployment/kafka_manager.ipynb",
+                        output_nb="dags/deployment/output/kafka_manager.ipynb",
+                        parameters={"KAFKA_BROKER_SERVER": server,"KAFKA_TOPIC_NAME":topic_name},
+                    )
+                    notebook_task >> t_data
+
+            
--- airflow/dags/deployment/task/task.py
+++ airflow/dags/deployment/task/task.py
@@ -2,12 +2,13 @@ from airflow.decorators import task
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_json, struct
-
+from deployment.data_handler.pipeline_builder import build_pipeline
 @task.python
-def read_count():
-    import os
-    os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 pyspark-shell'
-    print("==================================")
-    spark = SparkSession.builder.appName("test_data_explore").master("local[*]").getOrCreate()
-    df = spark.read.format('csv').option("header","true").load('/opt/airflow/dags/input/data/Waterbase_v2021_1_T_WISE6_DisaggregatedData.csv')
-    df.select(to_json(struct('*')).alias('value')).write.format("kafka").option("kafka.bootstrap.servers", "broker:29092").option("topic", "test_air").save()
+def pipeline_builder(config):
+    build_pipeline(config)
+    # import os
+    # os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 pyspark-shell'
+    # print("==================================")
+    # spark = SparkSession.builder.appName("test_data_explore").master("local[*]").getOrCreate()
+    # df = spark.read.format('csv').option("header","true").load('/opt/airflow/dags/input/data/Waterbase_v2021_1_T_WISE6_DisaggregatedData.csv')
+    # df.select(to_json(struct('*')).alias('value')).write.format("kafka").option("kafka.bootstrap.servers", "broker:29092").option("topic", "test_air").save()
