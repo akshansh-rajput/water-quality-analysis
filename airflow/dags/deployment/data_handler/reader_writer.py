67a50fc6c7b6f8c091f991e7b1a810926fb726c0
Akshansh Rajput <ext_akraj@arlafoods.com>
2023-07-05 09:32:00 +0300

    added doc string

--- airflow/dags/deployment/data_handler/reader_writer.py
+++ airflow/dags/deployment/data_handler/reader_writer.py
@@ -1,5 +1,15 @@
 
 def data_reader(spark, data_format, options = None, source_location = None):
+    """
+    Spark read method, accept source information i.e. format, options, path and return DataFrame.
+    Args:
+        spark           : Spark session to use spark functionality 
+        data_format     : Source format i.e. csv, kafka, json etc
+        options         : extra parameters to use by spark while reading from source
+        source_location : Source location. None in case kafka is source
+    Return:
+        DataFrame       : Spark Dataframe
+    """
     df_reader = spark.read.format(data_format)
     if options:
         df_reader = df_reader.options(**options)
@@ -9,6 +19,15 @@ def data_reader(spark, data_format, options = None, source_location = None):
         return df_reader.load()
     
 def data_writer(df, data_format, partitionBy = None, options = None, target = None):
+    """
+    Spark writer method, accept target information i.e. format, options, path and write data into the sink.
+    Args:
+        df              : Data to write 
+        data_format     : target format i.e. csv, kafka, json etc
+        partitionBy     : Which column should be use for partitioning 
+        options         : extra parameters to use by spark while writing to source
+        target          : target location. None in case kafka is target
+    """
     writer_df = df.write.format(data_format)
     if partitionBy:
         partition_cols = partitionBy.split(',')
